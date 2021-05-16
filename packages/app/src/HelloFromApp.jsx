2d21b9c4d5ba1cba9f21e1ee34067883b378d00a
Oleg Fed <post_over999@mail.ru>
2021-05-17 02:18:40 +0300

    Add application app

---
+++ packages/app/src/HelloFromApp.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const HelloFromApp = () => {
+    return (
+      <p>Hello from app directory</p>
+    );
+}
---
+++ packages/app/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { HelloWorldFromUIKit } from '@monorepo/uikit';
+
+render(<HelloWorldFromUIKit />, document.getElementById('root'));
+
